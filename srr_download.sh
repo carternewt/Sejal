eaf5e56b6dc053b3e4059a7c2976073f2dd94cb9
carternewt <150153088+carternewt@users.noreply.github.com>
2024-10-08 13:06:42 -0400

    Update srr_download.sh: debugging

--- srr_download.sh
+++ srr_download.sh
@@ -15,6 +15,7 @@ OUT='/work/lylab/cjn40747/Sejal'
 
 mkdir -p $OUT/reads
 while read line; do
+    line=$(echo "$line" | xargs)
     fasterq-dump -O $OUT/reads -t $OUT -e 12 "$line"
 done < $OUT/Paenibacillus_SRR.txt
     
