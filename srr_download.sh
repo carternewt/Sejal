858abd46a011bdb97e232babbe65c1006b6951a1
carternewt <150153088+carternewt@users.noreply.github.com>
2024-10-08 13:43:58 -0400

    Update srr_download.ssh: new approach to iterate

--- srr_download.sh
+++ srr_download.sh
@@ -14,8 +14,7 @@ ml SRA-Toolkit/3.0.3-gompi-2022a
 OUT='/work/lylab/cjn40747/Sejal'
 
 mkdir -p $OUT/reads
-while read line; do
-    echo "x" "$line" "x"
-    fasterq-dump "$line" -O $OUT/reads -t $OUT -e 12
-done < $OUT/Paenibacillus_SRR.txt
+
+cat $OUT/Paenibacillus_SRR.txt | xargs fasterq-dump -O $OUT/reads -t $OUT -e 12
+
     
