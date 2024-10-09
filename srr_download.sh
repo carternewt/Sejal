7039ce29a007db304d001d0faee2b2b2bb80a3b2
carternewt <150153088+carternewt@users.noreply.github.com>
2024-10-09 08:57:15 -0400

    Update srr_download.sh: Using --option-file subargument

--- srr_download.sh
+++ srr_download.sh
@@ -12,12 +12,12 @@
 ml SRA-Toolkit/3.0.3-gompi-2022a
 
 OUT='/work/lylab/cjn40747/Sejal'
-prefetch PRJNA1133887 -o $OUT/SRA/ -T sra
 mkdir -p $OUT/reads
+prefetch -o $OUT/SRA/ -T sra --option-file $OUT/Paenibacillus_SRR.txt
 #while read line; do
 #    line=$(echo $line | xargs)
 #    echo "$line"
-#    prefetch PRJNA1133887 -o $OUT/SRA/ -T sra
+#   prefetch -o $OUT/SRA/ -T sra --option-file $OUT/Paenibacillus_SRR.txt
 #    fasterq-dump $OUT/SRA/"$line".sra -O $OUT/reads -t $OUT -e 12
 #done < $OUT/Paenibacillus_SRR.txt
     
