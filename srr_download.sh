2967191a55a8ba6a40709fbdbd643761e0d1ac37
carternewt <150153088+carternewt@users.noreply.github.com>
2024-10-09 09:09:17 -0400

    Update srr_download.txt: prefetch works! Testing fasterq-dump loop.

--- srr_download.sh
+++ srr_download.sh
@@ -13,11 +13,8 @@ ml SRA-Toolkit/3.0.3-gompi-2022a
 
 OUT='/work/lylab/cjn40747/Sejal'
 mkdir -p $OUT/reads
-prefetch -O $OUT/SRA/ -T sra --option-file $OUT/Paenibacillus_SRR.txt
-#while read line; do
-#    line=$(echo $line | xargs)
-#    echo "$line"
-#   prefetch -o $OUT/SRA/ -T sra --option-file $OUT/Paenibacillus_SRR.txt
-#    fasterq-dump $OUT/SRA/"$line".sra -O $OUT/reads -t $OUT -e 12
-#done < $OUT/Paenibacillus_SRR.txt
+prefetch -O $OUT/SRA/ -T sra --option-file $OUT/test.txt
+find $OUT/SRA -type f -name *.sra | while read -r file; do
+    fasterq-dump "$file" -O $OUT/reads -t $OUT -e 12
+done
     
