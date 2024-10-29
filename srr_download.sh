a1dcbff3cd5657a458eaabdc87448d0504257df5
carternewt <150153088+carternewt@users.noreply.github.com>
2024-10-29 11:57:15 -0400

    Update srr_download.sh: Added seqkit to convert form fastq to fasta and gzip to compress

--- srr_download.sh
+++ srr_download.sh
@@ -10,17 +10,21 @@
 #SBATCH --output=/work/lylab/cjn40747/Sejal/logs/%j.out
 
 ml SRA-Toolkit/3.0.3-gompi-2022a
+ml SeqKit/2.5.1
 
 OUT='/work/lylab/cjn40747/Sejal'
 mkdir -p $OUT/reads
-prefetch -O $OUT/SRA/ -T sra --option-file $OUT/Paenibacillus_SRR.txt
+prefetch -O $OUT/SRA/ -T sra --option-file $OUT/test.txt
 find $OUT/SRA -type f -name *.sra | while read -r file; do
+    name=$(basename -s "$file")
     fasterq-dump "$file" -O $OUT/reads -t $OUT -e 12
+    seqkit fq2fa $OUT/reads/"$name".fastq -o $OUT/reads/"$name".fa 
+    gzip -k $OUT/reads/"$name".fa > $OUT/reads/"$name".fa.gz
 done
 
-touch $OUT/seqfile.txt
-find $OUT/assemblies -type f -name "*scaffolds.fasta" | while read -r file; do
-    file_name=$(basename "$file")
-    prefix="${file_name%.scaffolds.fasta}"
-    echo -e "$file_name\t$file" >> $OUT/seqfile.txt
-done
+#touch $OUT/seqfile.txt
+#find $OUT/assemblies -type f -name "*scaffolds.fasta" | while read -r file; do
+#    file_name=$(basename "$file")
+#    prefix="${file_name%.scaffolds.fasta}"
+#    echo -e "$file_name\t$file" >> $OUT/seqfile.txt
+#done
